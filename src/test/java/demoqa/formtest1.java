403db1f647e4e61f6e20adf9dbfeecd15c56ce8d
Offpilat <offpilat@gmail.com>
2023-08-07 14:40:32 +0300

    add void method

--- src/test/java/demoqa/formtest1.java
+++ src/test/java/demoqa/formtest1.java
@@ -2,11 +2,22 @@ package demoqa;
 
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class formtest1 {
     @Test
     void fillformtest() {
-        open("https://demoqa.com/text-box");
+        open("https://demoqa.com/automation-practice-form");
+        $("#firstName").setValue("Ivan");
+        $("#lastName").setValue("Ivanov");
+        $("#userEmail").setValue("test@mail.com");
+        $(byText("Male")).click();
+        $("#userNumber").setValue("+1920000000");
+        $("#dateOfBirthInput").setValue("11 Mar 2023");
+
     }
+
+
 }
